c6c0fbd9b9d3eb9313fa1c96e54a623475f5107d
Jason Morrissette <j.morrissette@jamdevs.com>
2017-01-26 11:08:06 -0800

    FEAT: Added Level Loader Script
    Added Script to load levels when a button is pushed ingame.

---
+++ LevelLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+    public void LoadLevel(string sceneToLoad)
+    {
+        // Loads Level
+        StartCoroutine(GameController.control.LoadLevel(sceneToLoad));
+    }
+}
