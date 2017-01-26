39ecbe6fb57372f61e5b92752d975af503835fb5
Jason Morrissette <j.morrissette@jamdevs.com>
2017-01-26 11:07:04 -0800

    FEAT: Added Skybox changer
    Added Skybox Changer script. This script will carry over skyboxes when
    changing scenes.

---
+++ SkyBoxChanger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyBoxChanger : MonoBehaviour
+{
+
+    public Material skybox;
+
+    // Use this for initialization
+    void Start()
+    {
+        RenderSettings.skybox = skybox;
+    }
+}
